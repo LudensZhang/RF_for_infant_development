f83330af1fdcde063d8e95496fc5cf08a34b5416
iceQHdrop <iceqhdrop@outlook.com>
2021-10-05 21:12:42 +0800

    CLF plot

--- Classification/CLF-model.py
+++ Classification/CLF-model.py
@@ -1,37 +1,26 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import tree
-from sklearn.model_selection import train_test_split
-# from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
-from sklearn.metrics import auc,roc_auc_score
-
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import label_binarize
+from plotnine import*
 
 # 构建随机森林
-def build_forest(X, y):       
+def build_forest(X_train, y_train, X_test, y_test):
     rfc = RandomForestClassifier()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     rfc = rfc.fit(X_train, y_train)
-    print('RFC score', rfc.score(X_test, y_test))
-    y_pred_prob = rfc.predict_proba(X_test)
-    auc_score = roc_auc_score(y_test, y_pred_prob, multi_class='ovo')
-    print('RFC auc score', auc_score)
+    y_pred_prob = rfc.predict(X_test)
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(5):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred_prob[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    return(roc_auc)
 
-
-# 构建决策树  
-def build_DTC(X, y):    
-    clf = tree.DecisionTreeClassifier()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf = clf.fit(X_train, y_train)
-    print('DTC score', clf.score(X_test, y_test))
-    y_pred_prob = clf.predict_proba(X_test)
-    auc_score = roc_auc_score(y_test, y_pred_prob, multi_class='ovo')
-    print('DTC auc score', auc_score)
-          
 
 if __name__ == '__main__':
-    # 数据转置
     C_abundance = pd.read_csv('Dataset/C-abundance.csv')
     C_abundance.set_index(C_abundance.columns[0], inplace=True)
     C_abundance = pd.DataFrame(C_abundance.values.T, columns = C_abundance.index, index = C_abundance.columns)
@@ -42,17 +31,44 @@ if __name__ == '__main__':
     V_abundance = pd.DataFrame(V_abundance.values.T, columns = V_abundance.index, index = V_abundance.columns)
     V_metadata = pd.read_csv('Dataset/V-metadata.csv')
     
-    # 分离特征、标签
     C_X, C_y = np.array(C_abundance), C_metadata['Env']
     C_features = C_abundance.columns
-    
+    C_y = label_binarize(C_y, classes=['B', '4M', '12M', '3Y', '5Y'])
+
     V_X, V_y = np.array(V_abundance), V_metadata['Env']
-    V_features = V_abundance.columns  
-    
-    build_forest(C_X, C_y)
-    build_DTC(C_X, C_y)
-    build_forest(V_X, V_y)
-    build_DTC(C_X, C_y)
+    V_features = V_abundance.columns
+    V_y = label_binarize(V_y, classes=['B', '4M', '12M', '3Y', '5Y'])
+
+    result = pd.DataFrame(columns=[0, 1, 2, 3, 4])
+    kf = KFold(n_splits=8)
+    for train_index, test_index in kf.split(C_X):
+        X_train, X_test = C_X[train_index], C_X[test_index]
+        y_train, y_test = C_y[train_index], C_y[test_index]
+        result = result.append(build_forest(X_train, y_train, X_test, y_test), ignore_index=1)
+    result['mode'] = 'caesarean section'
+
+    for train_index, test_index in kf.split(V_X):
+        X_train, X_test = V_X[train_index], V_X[test_index]
+        y_train, y_test = V_y[train_index], V_y[test_index]
+        result = result.append(build_forest(X_train, y_train, X_test, y_test), ignore_index=1)
+    result.fillna('vaginal delivery', inplace = True)
+    result.columns = ['NB', '4M', '12M', '3Y', '5Y', 'mode']
+    result = result.melt(id_vars='mode')
+
+    p = (ggplot(result, aes(x = 'variable', y = 'value', fill = 'mode'))+
+         geom_boxplot()+
+         scale_fill_manual(values = ["#DC143C", "#87CEEB"])+
+         theme_bw()+
+         xlim('NB', '4M', '12M', '3Y', '5Y')+
+         ylim(0,1)+
+         xlab('')+
+         ylab('AUROC')+
+         theme(text = element_text(size = 20),
+         legend_title = element_blank(),
+         legend_position = (0.78, 0.2),
+         legend_text = element_text(size=10)))
+    print(p)
+    p.save('8fold-result.png', height=5, width=8)
     
     
     
