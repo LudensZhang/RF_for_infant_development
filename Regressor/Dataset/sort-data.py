b07e99b3e371757ab7c6fe4611794e3859846feb
iceQHdrop <iceqhdrop@outlook.com>
2021-10-15 10:03:36 +0800

    update Regressor

--- Regressor/Dataset/sort-data.py
+++ Regressor/Dataset/sort-data.py
@@ -2,22 +2,22 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    abundance = pd.read_csv('RG-abundance.csv')
+    abundance = pd.read_csv('abundance.csv')
     metadata = pd.read_csv('RG-metadata.csv')
     filtered_labels = []
     
     for i in metadata.index:
-        if((metadata.loc[i, 'host_age'] > 0) and (metadata.loc[i, 'host_age'] <= 5)):
+        if((metadata.loc[i, 'host_age'] > 0) and (metadata.loc[i, 'host_age'] <= 2)):
             filtered_labels.append(i)
     
     filtered_metadata = metadata.loc[filtered_labels]
     filtered_metadata.drop(filtered_metadata.columns[0], axis=1, inplace=True)
     
     filtered_abundance = abundance[filtered_metadata['id']]
-    filtered_abundance.insert(0, 'Samples', abundance['samples'])
+    filtered_abundance.insert(0, 'Samples', abundance['Samples'])
     
     filtered_metadata.to_csv('metadata.csv', index=0)
-    filtered_abundance.to_csv('abundance.csv', index=0)
+    filtered_abundance.to_csv('abundance1.csv', index=0)
     
     
 
--- Regressor/RG-model.py
+++ Regressor/RG-model.py
@@ -2,23 +2,31 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from plotnine import*
-import skmisc
+from sklearn.metrics import mean_absolute_error 
+from sklearn.model_selection import train_test_split
 
 def buid_RFR(X, y):
     rfr = RandomForestRegressor()
-    rfr.fit(X, y)
-    y_pred = rfr.predict(X)
-    p = (ggplot(aes(x = y, y = y_pred))+
-                geom_point(color = '#66CCFF', alpha = 0.1)+
-                geom_smooth(method = 'lm', span = 1)+
-                theme_bw()+
-                xlab('Report Age')+
-                ylab('Predict Age'))
-    p.save('plot.png', width = 4, height = 4)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    rfr.fit(X_train, y_train)
+    y_pred = rfr.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    pd.DataFrame(list(zip(y_test, y_pred)), columns = ['y_test', 'y_pred']).to_csv('predict-result.csv', index=0)
+    print(mae)
+    # p = (ggplot(aes(x = y_test, y = y_pred))+
+    #             geom_point(color = '#66CCFF', alpha = 0.2)+
+    #             geom_smooth()+
+    #             theme_bw()+
+    #             xlim(0,2)+
+    #             ylim(0,2)+
+    #             xlab('Report Age')+
+    #             ylab('Predict Age')+
+    #             theme(text = element_text(size = 20)))
+    # p.save('plot.png', width = 5, height = 5)
 
 
 if __name__ == '__main__':
-    abundance = pd.read_csv('Dataset/abundance.csv')
+    abundance = pd.read_csv('Dataset/abundance1.csv')
     abundance.set_index(abundance.columns[0], inplace=True)
     abundance = pd.DataFrame(abundance.values.T, columns = abundance.index, index = abundance.columns)
     metadata = pd.read_csv('Dataset/metadata.csv')
@@ -27,10 +35,3 @@ if __name__ == '__main__':
     features = abundance.columns
 
     buid_RFR(X, y)
-<<<<<<< HEAD
-
-
-=======
-    
-    
->>>>>>> parent of 4ba19c1 (edit on pycharm)
